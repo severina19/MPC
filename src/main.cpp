d49d8fbc7d6b257859cb75ad8cdc1c3fc86ac4a6
anjaseverin <anjaseverin19@gmail.com>
2018-02-20 18:40:33 +0100

    bugfix

--- src/main.cpp
+++ src/main.cpp
@@ -32,15 +32,6 @@ string hasData(string s) {
   return "";
 }
 
-// Evaluate a polynomial.
-double polyeval(Eigen::VectorXd coeffs, double x) {
-  double result = 0.0;
-  for (int i = 0; i < coeffs.size(); i++) {
-    result += coeffs[i] * pow(x, i);
-  }
-  return result;
-}
-
 // Fit a polynomial.
 // Adapted from
 // https://github.com/JuliaMath/Polynomials.jl/blob/master/src/Polynomials.jl#L676-L716
